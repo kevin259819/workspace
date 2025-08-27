d075090d2f2ddb91fca496f215cc726804731806
emilianolarroca <emilianolarroca@gmail.com>
2025-08-27 20:11:02 -0300

    Mostrar nombre de usuario en la barra

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const nombreUsuario = localStorage.getItem("username"); // Traigo el nombre del usuario que está guardado en localStorage
+if (nombreUsuario) { // Si hay un usuario guardado, lo muestro en el span de la barra
+    const nombreElemento = document.getElementById("nombre-usuario"); // Selecciono el elemento <span> de la barra de navegación por su id
+    if (nombreElemento) {
+        nombreElemento.textContent = nombreUsuario; // Inserto el nombre dentro del <span> para que se muestre en pantalla
+    }
+}
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
